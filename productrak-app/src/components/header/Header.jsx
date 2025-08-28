3aa3b6a1088e27ba4ffa4e39847460ea9af6f8e0
ImrenR <imrenrahbay@gmail.com>
2025-08-28 23:51:20 +0100

    scss

--- productrak-app/src/components/header/Header.jsx
+++ productrak-app/src/components/header/Header.jsx
@@ -1,12 +1,19 @@
 
-
-const Header = ()=> {
+import HeaderStyle from './Header.module.scss'
+const Header = ({categories, title})=> {
   //!JS Alani
 
   return (
     //! JSX
-    <div>
-      <h1>Header</h1>
+    <div className={HeaderStyle.header} >
+      <h1>{title}</h1>
+      <div className={HeaderStyle.btn}>
+        {
+          categories.map((item, index)=>(
+            <button key={index} >{item}</button>
+          ))
+        }
+      </div>
     </div>
   )
 }
--- productrak-app/src/components/products/ProductList.jsx
+++ productrak-app/src/components/products/ProductList.jsx
@@ -3,7 +3,7 @@
 const ProductList = ()=> {
   return (
 <div>
-  <h1>Product List</h1>
+  {/* <h1>Product List</h1> */}
 </div>
   )
 }
