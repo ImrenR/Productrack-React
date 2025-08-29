218dd9800a947fe4dec9d4208e65fd67c89e5241
ImrenR <imrenrahbay@gmail.com>
2025-08-29 14:44:02 +0100

    ProductList &Card

--- productrak-app/src/components/products/ProductList.jsx
+++ productrak-app/src/components/products/ProductList.jsx
@@ -1,9 +1,24 @@
 
 
-const ProductList = ()=> {
+const ProductList = ({products})=> {
+  // console.log(props.products) //! we can go into the array withthis
+  
+  const {title,price, image,id } = products
+  
   return (
 <div>
-  {/* <h1>Product List</h1> */}
+  {
+    products.map((item)=> {
+      return (
+        <div>
+              {item.id}
+              <div>{item.title}</div>
+               <img src={item.image}/>
+          
+        </div>
+      )
+    })
+  }
 </div>
   )
 }
