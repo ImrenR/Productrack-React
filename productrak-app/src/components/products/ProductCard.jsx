7d81519fecd10f2d31765323d9a72509299dcbf7
ImrenR <imrenrahbay@gmail.com>
2025-08-31 18:17:26 +0100

    scss update

--- productrak-app/src/components/products/ProductCard.jsx
+++ productrak-app/src/components/products/ProductCard.jsx
@@ -7,7 +7,7 @@ const ProductCard = ({price, title, image, id}) => {
      <div className="price"  >
       <h3>{price}$</h3>
       </div>
-      <img src={Image} alt="" />
+      <img src={image} alt="" />
       <div className="card_over">
         <h2>{title}</h2>
       </div>
