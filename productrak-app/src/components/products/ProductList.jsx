c3b1f04cafaf57faf99187d8e15644805a6c9a89
ImrenR <imrenrahbay@gmail.com>
2025-08-31 18:42:51 +0100

    update

--- productrak-app/src/components/products/ProductList.jsx
+++ productrak-app/src/components/products/ProductList.jsx
@@ -2,8 +2,7 @@ import ProductCard from "./ProductCard"
 import "./Product.scss"
 
 const ProductList = ({products})=> {
-  // console.log(props.products) //! we can go into the array withthis
-  
+
   return (
 <div className="product-list">
   {
